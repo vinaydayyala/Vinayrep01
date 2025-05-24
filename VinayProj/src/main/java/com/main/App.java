3ae6746eb68aef30d27a97e06897a0d8056813a4
pendli raju <pendli raju@PENDLIRAJU>
2025-05-24 12:44:23 -0700

    update in app.class  met2  --vinay

--- VinayProj/src/main/java/com/main/App.java
+++ VinayProj/src/main/java/com/main/App.java
@@ -1,13 +1,15 @@
 package com.main;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        met2();
+    }
+    
+    public static void met2(  )
+    {
+    	System.out.println("Static metHod !");
     }
 }
