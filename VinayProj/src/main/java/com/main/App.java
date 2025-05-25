be8197c0eca4ced8be34833a8baaa515a633fc70
pendli raju <pendli raju@PENDLIRAJU>
2025-05-24 19:50:18 -0700

    commit

--- VinayProj/src/main/java/com/main/App.java
+++ VinayProj/src/main/java/com/main/App.java
@@ -5,11 +5,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        met2();
-    }
-    
-    public static void met2(  )
-    {
-    	System.out.println("Static metHod !");
     }
 }
